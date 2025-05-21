1911b5fd2258e3461a0c3bf67ae3855d2c2093ac
ritu66722999 <botofficial233@gmail.com>
2025-05-21 19:15:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'ＤＡＲＫ⚔ＫＩＬＬＥＲ',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ＤＡＲＫ⚔ＫＩＬＬＥＲ",
+  botOwner: 'ＤＡＲＫ⚔ＫＩＬＬＥＲ', // Your name
+  ownerNumber: '918910051810', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
